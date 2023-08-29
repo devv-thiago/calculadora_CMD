d2ea3cb74bcbe24e1de9f3e94d578ac62c1da34b
devv-thiago <tfofano3@gmail.com>
2023-08-29 20:17:15 -0300

    finalizando projeto versao 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ global using global::System.Threading;
 global using global::System.Threading.Tasks;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Calculadora;
 
 internal class Program
@@ -24,55 +26,55 @@ internal class Program
         double numero1;
         double numero2;
 
-
         while (continua)
-
         {
             Console.WriteLine("Escolha a operacao");
             Console.WriteLine("1 - Adicao\n2 - divisao\n3 - multiplicacao\n4 - substracao\n5 - sair");
             Menu resposta = (Menu)int.Parse(Console.ReadLine());
-
             switch (resposta)
             {
                 case Menu.adicao:
-                    Console.WriteLine("Digite o primeiro número: ");
+                    Console.Write("Digite o primeiro número: ");
                     numero1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número: ");
+                    Console.Write("Digite o segundo número: ");
                     numero2 = double.Parse(Console.ReadLine());
                     resultado = calculadora.adicao(numero1, numero2);
-                    Console.WriteLine(resultado);
-                   
+                    Console.Write($"Resultado: {resultado}");
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.divisao:
-                    Console.WriteLine("Digite o primeiro número: ");
+                    Console.Write("Digite o primeiro número: ");
                     numero1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número: ");
+                    Console.Write("Digite o segundo número: ");
                     numero2 = double.Parse(Console.ReadLine());
                     resultado = calculadora.divisao(numero1, numero2);
-                    Console.WriteLine(resultado);
-                    
+                    Console.Write($"Resultado: {resultado}");
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.multiplicacao:
-                    Console.WriteLine("Digite o primeiro número: ");
+                    Console.Write("Digite o primeiro número: ");
                     numero1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número: ");
+                    Console.Write("Digite o segundo número: ");
                     numero2 = double.Parse(Console.ReadLine());
                     resultado = calculadora.multiplicacao(numero1, numero2);
-                    Console.WriteLine(resultado);
-                    
+                    Console.Write($"Resultado: {resultado}");
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.subtracao:
-                    Console.WriteLine("Digite o primeiro número: ");
+                    Console.Write("Digite o primeiro número: ");
                     numero1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número: ");
+                    Console.Write("Digite o segundo número: ");
                     numero2 = double.Parse(Console.ReadLine());
                     resultado = calculadora.subtracao(numero1, numero2);
-                    Console.WriteLine(resultado);
-                    
+                    Console.Write($"Resultado: {resultado}");
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.sair:
                     continua = false;
-                    
                     break;
             }
         }
